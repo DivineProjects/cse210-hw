6d40d93c1b352fc6b740dc3f1104686a204ee663
divineProjects <divinejigu@gmail.com>
2024-05-22 06:17:43 +0200

    Added activities file

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,49 @@
+using System;
+
+public class Activity
+{
+    private string _name;
+    private string _description;
+    private int _duration;
+
+    public Activity(string name, string description, int duration)
+    {
+        _name = name;
+        _description = description;
+        _duration = duration;
+    }
+
+    public void DisplayStartingMessage()
+    {
+        Console.WriteLine($"{_name}: {_description} ({_duration} minutes)");
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine($"{_name}: {_description} ({_duration} minutes)");
+    }
+
+    public void showSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            System.Threading.Thread.Sleep(1000);
+            Console.Write("\b \b"); // Erase the + character
+            Console.Write("."); // Replace it with the - character
+        }
+        Console.WriteLine();
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write($"{i} ");
+            System.Threading.Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+
+}
