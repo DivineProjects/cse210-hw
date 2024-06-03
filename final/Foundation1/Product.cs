57794f6478d7ead08db0e4b556a95365d7018f4d
divineProjects <divinejigu@gmail.com>
2024-06-03 11:52:57 +0200

    created product class

---
+++ final/Foundation1/Product.cs
@@ -0,0 +1,43 @@
+using System;
+
+public class Product
+{
+    private string _name; 
+    private string _productId;
+    private float _price; 
+    private int _quantity;
+
+    public Product(string name, string productId, float price)
+    {
+        _name = name;
+        _productId = productId;
+        _price = price;
+        _quantity = 0;
+    }
+
+    public int SetProductQuantity(int quantity)
+    {
+        _quantity = quantity;
+    }
+
+    public string GetProductName()
+    {
+        return _name;
+    }
+
+    public float GetPrice()
+    {
+        return _price;
+    }
+
+    public string GetProductId()
+    {
+        return _productId;
+    }
+
+    public float ProductTotalCost()
+    {
+        return _price * _quantity;
+    }
+
+}
